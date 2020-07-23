8e5bf608184b810e258b4e85288ae4c19ae63ac8
Huncho <yasharya11@gmail.com>
2020-07-23 02:26:22 -0700

    Setting up some base pages

---
+++ Portfolio.Core/Testimonial.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Portfolio.Core
+{
+    public class Testimonial
+    {
+
+    }
+}
--- portfolio-site/Controllers/HomeController.cs
+++ portfolio-site/Controllers/HomeController.cs
@@ -6,21 +6,52 @@ namespace portfolio_site.Controllers
 {
     public class HomeController : Controller
     {
+        //START TESTING STUFF
+        public IActionResult Clicker()
+        {
+            ViewData["Message"] = "Made to test if the database connection works";
+            return View();
+        }
+        //END TESTING STUFF
+
+
+        //This is the website landing page
         public IActionResult Index()
         {
+            ViewData["Message"] = "This is your basic info";
             return View();
         }
 
-        public IActionResult About()
+        //
+        public IActionResult Testimonials()
         {
-            ViewData["Message"] = "PLace holder data about yourself";
-
+            ViewData["Message"] = "This is where testimonials live";
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Contact info, maybe cell and email... plus github";
+            ViewData["Message"] = "Contact info for recruiters";
+
+            return View();
+        }
+
+        public IActionResult SkillsAndExp()
+        {
+            ViewData["Message"] = "Long list of skills and experience";
+
+            return View();
+        }
+
+        public IActionResult Bio()
+        {
+            ViewData["Message"] = "Some info about yourself";
+
+            return View();
+        }
+        public IActionResult Practice()
+        {
+            ViewData["Message"] = "A collection of practice peices";
 
             return View();
         }
